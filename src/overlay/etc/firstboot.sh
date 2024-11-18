790a204124c18f26d28b63f11d182906445b58c3
diatribes <diatribes@gmail.com>
2024-11-17 19:43:28 -0500

    build/input/write-cpio.sh -> src/overlay/root/bin/write-cpio.sh

--- src/overlay/etc/firstboot.sh
+++ src/overlay/etc/firstboot.sh
@@ -3,7 +3,7 @@ set -e
 
 # this script is in upinevm, and in built images it is filtered out in write-cpio.sh
 
-if [ -f /mnt/input/write-cpio.sh ]; then
+if [ -f /root/bin/write-cpio.sh ]; then
     # update and upgrade
     apk upgrade --no-cache
 
@@ -11,10 +11,10 @@ if [ -f /mnt/input/write-cpio.sh ]; then
     apk add --no-cache `cat /mnt/input/packages.conf`
 
     # write cpio and exit
-    chmod +x /mnt/input/write-cpio.sh
-    /mnt/input/write-cpio.sh
+    chmod +x /root/bin/write-cpio.sh
+    /root/bin/write-cpio.sh
 else
-    echo "firstboot: no write-cpio.sh, exiting"
+    echo "firstboot: no /root/bin/write-cpio.sh, exiting"
     find / ! -path "/mnt/*" ! -path "/sys/*" ! -path "/proc/*" ! -path "/dev/*" ! -path "/etc/firstboot" ! -path "/etc/firstboot.sh"
     find / -name firstboot
     find / -name firstboot.sh
