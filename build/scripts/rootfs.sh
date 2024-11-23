95c70c247c694861769eea93bfad01b2b1dac558
diatribes <diatribes@gmail.com>
2024-11-23 02:43:48 -0500

    chroot pkg install

--- build/scripts/rootfs.sh
+++ build/scripts/rootfs.sh
@@ -1,15 +1,24 @@
 #!/bin/bash 
-curl -o "${CACHEPATH}/alpine-rootfs.tar.gz" "https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/x86_64/alpine-minirootfs-3.20.3-x86_64.tar.gz"
+
 cd "${CACHEPATH}"
-mkdir -p rootfs
-cd rootfs
-tar xvf ../alpine-rootfs.tar.gz
+
+# get apk.static tool
+curl -o "${CACHEPATH}"/apk.static https://gitlab.alpinelinux.org/api/v4/projects/5/packages/generic/v2.14.0/x86_64/apk.static
+chmod +x ./apk.static
+
+# init rootfs
+mkdir -p "${CACHEPATH}"/rootfs
+./apk.static --arch x86_64 -X http://dl-cdn.alpinelinux.org/alpine/latest-stable/main/ -U --allow-untrusted --root ./rootfs --initdb add alpine-base `cat "${SRCPATH}"/packages.conf`
+
+# add custom files
 mkdir -p "${SRCPATH}/overlay/usr/bin"
 cp -v "${CACHEPATH}/sdhcp/sdhcp" "${SRCPATH}/overlay/usr/bin/sdhcp"
 cp -v "${SRCPATH}/dumb-init/dumb-init" "${SRCPATH}/overlay"
 cp -v "${SRCPATH}/carl-exit/carl-exit" "${SRCPATH}/overlay"
-cp -r ${SRCPATH}/overlay/* .
+cp -r ${SRCPATH}/overlay/* "${CACHEPATH}"/rootfs
 chmod +x dumb-init
 chmod +x carl-exit
-find . | cpio -o -H newc > "${OUTPUTPATH}"/rootfs.cpio
+
+cd "${CACHEPATH}"/rootfs
+find . | cpio -o -H newc > "${OUTPUTPATH}"/rootfs-new.cpio
 
