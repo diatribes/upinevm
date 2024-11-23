159fd323c75cf190c798900f41324ef11554604c
Theta Nil <20186755+thetanil@users.noreply.github.com>
2024-11-23 23:42:13 +0100

    overridable config vars

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-export BUILDROOT=`realpath .`
-export CACHEPATH=`realpath ./build/cache`
-export KERNELVER=6.12
-export SRCPATH=`realpath src`
-export OUTPUTPATH=`realpath ./build/output`
-export INPUTPATH=`realpath ./build/input`
-export INSTALLMEM=512M
-export RUNMEM=512M
-export KERNELCONFIG=`realpath ./src/qemu-kernel-config`
+export BUILDROOT="${BUILDROOT:-$(realpath .)}"
+export CACHEPATH="${CACHEPATH:-$(realpath ./build/cache)}"
+export KERNELVER="${KERNELVER:-6.12}"
+export SRCPATH="${SRCPATH:-$(realpath src)}"
+export OUTPUTPATH="${OUTPUTPATH:-$(realpath ./build/output)}"
+export INPUTPATH="${INPUTPATH:-$(realpath ./build/input)}"
+export INSTALLMEM="${INSTALLMEM:-512M}"
+export RUNMEM="${RUNMEM:-512M}"
+export KERNELCONFIG="${KERNELCONFIG:-$(realpath ./src/qemu-kernel-config)}"#!/bin/sh
 
