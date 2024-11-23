0a1861fc90b7bd11fa95a122badc9fc6e3ce1007
Theta Nil <20186755+thetanil@users.noreply.github.com>
2024-11-23 22:38:50 +0100

    manual merge

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,10 +23,32 @@ mkdir -pv "${OUTPUTPATH}"
 ./build/scripts/carl-exit.sh
 sudo -E ./build/scripts/rootfs.sh
 
-if [ ! -f "${OUTPUTPATH}"/bzImage ]; then
-    echo "bzImage: not found, building..."
-    export CURRENTKERNELFILENAME="bzImage"
-    export CURRENTKERNELCONFIG=${KERNELCONFIG}
-    ./build/scripts/kernel.sh
+export CURRENTKERNELCONFIG=${KERNELBOOTSTRAPCONFIG}
+./build/scripts/kernel.sh
+
+# Exit early if UPINEVM_NOLAUNCH is set
+if [ -n "$UPINEVM_NOLAUNCH" ]; then
+    echo "UPINEVM_NOLAUNCH is set. Exiting script early."
+    exit 0
 fi
 
+qemu-system-x86_64 \
+    -display none \
+    -no-reboot \
+    -no-user-config \
+    -nodefaults \
+    -m ${INSTALLMEM} \
+    -kernel "${OUTPUTPATH}/bzImage" \
+    -cpu host \
+    -enable-kvm \
+    -nographic \
+    -serial none -device isa-serial,chardev=s1 \
+    -chardev stdio,id=s1,signal=off \
+    -append "panic=-1 notsc" \
+    -initrd "${OUTPUTPATH}/rootfs.cpio" \
+    -nic user,model=virtio-net-pci \
+    -virtfs local,path=${INPUTPATH},mount_tag=host0,security_model=none,id=host0 \
+    -virtfs local,path=${OUTPUTPATH},mount_tag=host1,security_model=none,id=host1
+
+export CURRENTKERNELCONFIG=${KERNELCONFIG}
+./build/scripts/kernel.sh
